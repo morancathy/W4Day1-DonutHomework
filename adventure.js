75cf0d52b89a1065f1351ac2e51ed01f2b7c6f33
Cat <morancathy@gmail.com>
2021-05-16 22:15:57 -0500

    created our hero dougie

--- adventure.js
+++ adventure.js
@@ -1,1 +1,35 @@
-console.log("Does this mean my files are connected?");
+//W4Day1 - Homework - Adventure of Dougie the Donut
+
+class Hero {
+  constructor(name){
+    this.name = name;
+    this.health = 100;
+    this.weapons = {
+      sprinkleSpray: 5,
+      sugarShock: 10
+    }
+    this.catchPhrases = ['i\'m fresher than day old pizza','you can\'t count my calories'];
+  };
+
+  talkSass(){
+    const quoteEl = document.querySelector('#random-quote');
+    quoteEl.textContent = `"${this.catchPhrases[Math.floor(Math.random() * this.catchPhrases.length)]}"`
+  };
+
+  annouceHealth(){
+    console.log(this.health);
+  }
+
+  fight(){
+    console.log(`I\'m ready to rumble!`);
+    const fightQuoteEl = document.querySelector('#fight-quote');
+    fightQuoteEl.textContent = "I'm ready to rumble!";
+
+  }
+
+};
+
+const Dougie = new Hero("Dougie");
+Dougie.talkSass();
+Dougie.fight();
+console.log("word up");
